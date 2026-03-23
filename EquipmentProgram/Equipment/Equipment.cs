6c9a4e110916e7bd7a0f0c8620ea53f0b94d375d
KolosLD <s30211@pjwstk.edu.pl>
2026-03-23 14:47:00 +0100

    Initial project

---
+++ EquipmentProgram/Equipment/Equipment.cs
@@ -0,0 +1,19 @@
+﻿namespace EquipmentProgram;
+
+public abstract class Equipment
+{
+    public string Id { get; private set; }
+    public string Name { get; set; }
+    public bool AvailabilityStatus { get; set; }
+
+
+    protected Equipment(string name)
+    {
+        Id = Guid.NewGuid().ToString().Substring(0, 8);
+        Name = name;
+        AvailabilityStatus = true;
+    }
+
+    public abstract string GetDetails();
+    
+}
