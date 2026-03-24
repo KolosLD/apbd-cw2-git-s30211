742bc50c25e3f01a708960e3413f818b1dfe9917
KolosLD <s30211@pjwstk.edu.pl>
2026-03-24 22:18:34 +0100

    Add abstract class User

---
+++ EquipmentProgram/Equipment/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace EquipmentProgram;
+
+public abstract class User
+{
+ public string Id { get; private set; }   
+ public string FirstName { get; set; }
+ public string LastName { get; set; }
+  
+ public abstract int MaxRentalLimit { get; }
+ 
+ protected User(string id, string firstName, string lastName)
+ {
+ Id = Guid.NewGuid().ToString().Substring(0, 8);
+ FirstName = firstName;
+ LastName = lastName;
+ }
+ 
+ 
+}
