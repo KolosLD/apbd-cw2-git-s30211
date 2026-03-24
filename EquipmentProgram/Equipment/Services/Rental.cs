fa0d8824326e92f72eef40ba3106d17a9e433ae6
KolosLD <s30211@pjwstk.edu.pl>
2026-03-24 23:00:29 +0100

    Add RentalService class

--- EquipmentProgram/Equipment/Services/Rental.cs
+++ EquipmentProgram/Equipment/Services/Rental.cs
@@ -3,28 +3,29 @@
 public class Rental
 {
     public string Id { get; private set; }
-    public User User { get; set; }
-    public Equipment Equipment { get; set; }
+    public string UserId { get; private set; }
+    public string EquipmentId { get; private set; }
     public DateTime RentalDate { get; set; }
     public DateTime DueDate { get; set; }
     public DateTime? ReturnDate { get; set; }
 
 
-    public Rental(User user, Equipment equipment, int days)
+    public Rental(string userId, string equipmentId, int days)
     {
         Id = Guid.NewGuid().ToString().Substring(0, 8);;
-        User = user;
-        Equipment = equipment;
+        UserId = userId;
+        EquipmentId = equipmentId;
         RentalDate = DateTime.Now;
         DueDate = DateTime.Now.AddDays(days);
     }
-
-    public decimal CalculatePenalty(decimal penaltyPerDay)
+    
+    
+    public void MarkAsReturned()
     {
-        if (!ReturnDate.HasValue || ReturnDate <= DueDate) return 0;
-        var delay = (ReturnDate.Value - DueDate).Days;
-        return delay * penaltyPerDay;
+        ReturnDate = DateTime.Now;
     }
-
+    
+    public bool IsOverdue => ReturnDate == null && DateTime.Now > DueDate;
+    public bool IsReturned => ReturnDate != null;
 
 }
---
+++ EquipmentProgram/Equipment/Services/RentalService.cs
@@ -0,0 +1,50 @@
+﻿namespace Equipment;
+
+public class RentalService
+{
+    private readonly List<Rental> _rentals = new();
+    private const decimal DailyPenaltyRate = 10.0m;
+
+
+    public string RentItem(User user, Equipment equipment)
+    {
+        if (!equipment.AvailabilityStatus)
+            return "Błąd: Sprzęt jest niedostępny ";
+        
+        int activeRentalsCount = _rentals.Count(r => r.UserId == user.Id && !r.IsReturned);
+        if (activeRentalsCount >= user.MaxRentalLimit)
+            return $"Błąd: użytkownik {user.LastName} przekroczył swój limit ({user.MaxRentalLimit}).";
+        
+        
+        var newRental = new Rental(user.Id, equipment.Id, 7); 
+        _rentals.Add(newRental);
+        equipment.AvailabilityStatus = false; 
+
+        return $"Skutecznie: {equipment.Name} wydawane użytkownikowi {user.LastName}. Identyfikator wynajmu: {newRental.Id}";
+    }
+    
+    public string ReturnItem(Rental rental, Equipment equipment)
+    {
+        rental.MarkAsReturned();
+        equipment.AvailabilityStatus = true; 
+
+        decimal penalty = CalculatePenalty(rental);
+        if (penalty > 0)
+            return $"\nZwrócono towar. Naliczono opłatę za zwłokę.: {penalty} pln.";
+
+        return "Przedmiot został zwrócony na czas. Bez żadnych kar.";
+    }
+
+    private decimal CalculatePenalty(Rental rental)
+    {
+        if (rental.ReturnDate <= rental.DueDate) return 0;
+        
+        var delay = (rental.ReturnDate.Value - rental.DueDate).Days;
+        return delay * DailyPenaltyRate; 
+    }
+    
+    public List<Rental> GetOverdueRentals() => _rentals.Where(r => r.IsOverdue).ToList();
+    
+    
+    
+}
